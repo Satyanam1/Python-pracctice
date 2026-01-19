0d7bd740877c3e5d2f8772c7d1170e83a3233216
Satyanam1 <satya949496@gmail.com>
2026-01-20 00:14:02 +0530

    learn operator and solved basic questions based on ternary operator

---
+++ operator.py
@@ -0,0 +1,42 @@
+# Arithmetic operator -> -,+,*,/,//,%,**
+# Assignment operator
+# Comparison operator
+# Logical operator
+# Identity optitor
+# Membership operator
+# bit wise operator
+
+
+# area of rectangle
+
+""" length = int(input("enter length: "))
+width = int(input("Enter width: "))
+rectangle=length*width
+print(rectangle) """
+
+
+# Area of circle
+
+""" radius = int(input("enter area: "))
+print("Area of circle: ")
+print(3.14*radius*radius) """
+
+# Area of triangle
+
+""" base= int(input("enetr a number: "))
+height= int(input("enetr a number: "))
+print((base*height)/2) """
+
+
+# celcius to fehrenheite
+
+""" celcius = int(input("enter celcius: "))
+fahrenheite =((celcius*9/5)+32)
+print(fahrenheite) """
+
+# fahrenheite to celcius
+
+fahrenheite = int(input("Enter fahrenheite: "))
+celcius = ((fahrenheite-32)*5/9)
+print(celcius)
+
---
+++ ternaryOperator.py
@@ -0,0 +1,50 @@
+# terenary operator
+
+
+# 1
+""" age = int(input("Enter age: "))
+print("Eligible for vote") if age>=18 else print("Not eligible for vote") """
+
+# 2
+""" num = int(input("Enter any number: "))
+print("Negative") if num<0 else print("Positive") """
+
+# 3
+""" password = int(input("Enter password: "))
+print("Correct") if password == 1234 else print("Incorrect") """
+# 4
+""" email=input("enter your email: ")
+password = int(input("enter your password: "))
+print("correct") if (email == "satya9494@gmail.com") and (password==1234) else print("Incorrect Email and Password") """
+
+# 5
+""" num = int(input("Enter number: "))
+print("Divisible by 5 and 2") if num%5==0 and num%2==0 else print("Not Divisible by 5 and 2") """
+
+# 6
+""" num = int(input("Enter number: "))
+print("Divisible by 4 and above 50") if num%4==0 and num>50 else print("Not divisible by 4 and above 50") """
+#9
+""" num = int(input("Enter number: "))
+print("Divisible by 3 and 6") if num%3==0 and num%6==0 else print("Not Divisible by 3 and 6") """
+
+
+# 8 
+""" 
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(num1) if num1<num2 else print(num2) """
+
+
+
+# 7
+""" char = input("Enter any character: ")
+print("vowel") if (char =="a") or (char =="e") or (char =="i") or (char =="o") or (char =="u") else print("Consonenet") """
+
+
+
+# membership operator
+
+# char = input("enter a character: ")
+# print(char in 'aeiouAEIOU')
+
