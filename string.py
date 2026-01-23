8a1bb07263e93ca1a23f6b1bc5edaeb48dd57fd6
Satyanam1 <satya949496@gmail.com>
2026-01-23 23:38:37 +0530

    Add string

---
+++ string.py
@@ -0,0 +1,42 @@
+# String --> String is a data type that stores a sequence of characters.
+#   •immutable
+#   •we can write in single quotes('') or double quotes("") or """ """  'hello' or "hello" or """hello"""
+#   •we can use \n to print in the next line
+#   •we can use \t 
+    
+# concatination in string
+""" str1 = "hello"
+str2 =  "satyanam"
+final = str1+str2
+print(final) """
+
+
+# name = "Hello Satyanam Ray"
+# print(name.capitalize()) # capitalize first character
+# print(name.title())
+# print(name.split())
+# print(len(name))
+# print(name.find("S"))
+# print(name.replace("S","L")) 
+# print(name.count("S"))
+# myans = "".join(name)
+# print(myans)
+# print(name)
+
+
+# numbers="bhopal"
+# numbers=numbers[-1]+ numbers[1:-1]+ numbers[0]
+# print(numbers)
+
+
+number="12346"
+number =(int(number[0])+int(number[3]))
+print(number) # 1+4 = 5
+
+
+# text="Hello Satyanam bhai"
+# print("bhai" in text)
+
+
+
+
