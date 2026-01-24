6ae9d46de88e9221773ac6addc538f227e54c903
Satyanam1 <satya949496@gmail.com>
2026-01-24 23:18:22 +0530

    Add list and tuples

---
+++ ListTuple.py
@@ -0,0 +1,45 @@
+data = [1,2,7,8,25,12,25]
+# data1=data.copy()
+# data.pop()
+# print(data)
+# data.append(21)
+# print(data)
+
+# data.extend([12,10,12])
+# print(data)
+
+# data.pop()
+# print(data)
+
+# data.pop(0)
+# print(data)
+
+# data.sort()
+# print(data)
+
+
+data.reverse()
+print(data)
+data.sort(reverse=True)
+print(data)
+
+
+
+
+# ans =data.index(12)
+# print(ans)
+
+# ans1=data.count(12)
+# print(ans1)
+
+
+# data.reverse()
+# print(data)
+
+# data.remove(12)
+# print(data)
+
+# data.clear()
+# print(data)
+# del data()
+# print(data)
