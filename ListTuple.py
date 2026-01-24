839454c36c62fb7023d97bd961f67a692cd37acf
Satyanam1 <satya949496@gmail.com>
2026-01-24 23:27:49 +0530

    Add list and tuple

--- ListTuple.py
+++ ListTuple.py
@@ -1,3 +1,7 @@
+# List --> Lists are used to store multiple items in a single variable.
+# •List are created using square brackets []
+# •List items are ordered, changeable, and allow duplicate values.
+
 data = [1,2,7,8,25,12,25]
 # data1=data.copy()
 # data.pop()
@@ -43,3 +47,51 @@ print(data)
 # print(data)
 # del data()
 # print(data)
+
+
+# Tuple () --> Tuples are used to store multiple items in a single variable.
+#    •Tuples are written with round brackets.
+#    •A tuple items are ordered, unchangeable and allow duplicate values.
+
+
+# data =()
+# data = tuple()
+
+# data = (1,3,4,7,8,5)
+# data[0]= data[0]+12
+# print(data)
+# print(data[0])
+
+# data = (1,1,2,4,3,7,5)
+# data=sorted(data)
+# data=tuple(data)
+# print(data)
+
+""" print(len(data))
+print(max(data))
+print(min(data))
+print(sum(data))
+print(data.count(1))
+print(data.index(6)) """
+
+# sum
+# largest
+# second large
+# small
+# second smallest
+
+num = [1,5,3,4,2]
+# print(sum(num))
+
+# print(max(num))
+
+# print(min(num))
+
+num.sort()
+# print(num[-2])
+
+
+print(num[1])
+
+
+
