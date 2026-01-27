90d52e2efe1e660cbd5391452f7bea667f8c03e3
Satyanam1 <satya949496@gmail.com>
2026-01-27 18:49:37 +0530

    add range

---
+++ range.py
@@ -0,0 +1,65 @@
+
+# print(start,range,end)
+
+# print(list(range(1,11,1)))
+# print(list(range(1,11)))
+# print(list(range(-11,-1,1)))
+# print(list(range(11,1,-1)))
+
+
+# name = "python"
+# print(name[1:5:1])
+# print(name[1:5:])
+# print(name[1::])
+# print(name[1::])
+# print(name[1:5:-1])  # blank
+# print(name[-1:-5:-1])    
+# print(name[5:1:-1])    
+# print(name[1:-4:1]) 
+# print(name[-1:1:-1]) 
+# print(name[::-1]) 
+
+# data="python"
+# data=data[-1]+ data[1:-1]+ data[0]
+# print(data)
+
+# name="python"
+# print(name[-1]+name[1:-1]+name[0])
+
+
+# num=int(input("Enter a number: "))
+# s=str(num)
+# rev=""
+# for i in range(len(s)-1,-1,-1):
+#     rev+=s[i]
+# print(rev) 
+
+
+# num = input("Enter a number: ")
+# rev=""
+# for i in range(len(num)-1,-1,-1):
+#     rev+=num[i]
+# print(rev)
+
+
+
+# num=(input("Enter a number: "))
+# rev=""
+# for i in range(len(num)-1,-1,-1):
+#     rev+=num[i]
+# if num==rev:
+#     print("palindrome")    
+# else:
+#     print("Not palindrome")
+
+
+# num = input("Enter: ")
+# rev=""
+# for i in range(len(num)-1,-1,-1):
+#     rev+=num[i]
+# if num==rev:
+#     print("palindrome")
+# else:
+#     print("Not palindrome")    
+   
+
