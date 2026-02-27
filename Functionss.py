a5f37b80b15d94f95defe95c240b51cdb962d28f
Satyanam1 <satya949496@gmail.com>
2026-02-27 20:53:29 +0530

    Function

---
+++ Functionss.py
@@ -0,0 +1,83 @@
+""" x='Hello'
+print(x)
+print(print(x)) """
+
+""" def new(a,b):
+    c=a+b
+    return c
+p=int(input("Enter a number: "))    
+q=int(input("Enter second number: "))    
+    
+res=new(p,q)
+print(res) """
+
+# x=print("hello")
+# print(x)
+
+# s="Python"
+# x=print(max(s))
+# print(x)
+
+# s='abcde'
+""" max=0
+for i in s:
+    x=ord(i)
+    if max<x:
+        max=x
+
+print(chr(max)) """
+               
+""" def maxvalue(s):
+    max=0
+    for i in s:
+        x=ord(i)
+        if max<x:
+            max=x
+    print(chr(max))        
+a=input("Enter any string: ") 
+maxvalue(a) """
+
+
+ 
+""" def minimum(s):
+    min=255
+    for i in s:
+        x=ord(i)
+        if min>x:
+            min=x
+
+    print(chr(min))
+a=input("Enter string: ")
+minimum(a)  """
+
+
+""" def secondLargest(lst):
+    max=lst[0]
+    secondMax=lst[0]
+    for i in lst:
+        if i>max:
+            secondMax=max
+            max=i
+        if i<secondMax and i>max:
+              secondMax=i
+    return secondMax
+
+listt = list(map(int,input("Enter a list: ")))
+print(secondLargest(listt))  """          
+
+
+# s="Python"
+# l=0
+# for i in s:
+#      l+=1
+# print(l) 
+
+""" def len(s):
+     l=0
+     for i in s:
+          l+=1
+     print(l)
+
+a=input("Enter string: ")
+len(a) """
+
