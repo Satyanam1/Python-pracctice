38210f067174b783107e657081846be00bba1ffa
Satyanam1 <satya949496@gmail.com>
2026-03-11 22:21:35 +0530

    filter and reduce

--- oops.py
+++ oops.py
@@ -1,4 +1,6 @@
 
+
+from functools import reduce
 # map
 """ l1=[1,2,3]
 l2=[2,4,5,6,7]
@@ -12,10 +14,20 @@ print(tuple(res)) """
 
 # filter
 
-l=[4,6,8]
+""" l=[4,6,8]
 
 def even(n):
     if n%2==0:
         return n
 res = list(filter(even,l))
+print(res) """
+
+
+# reduce
+
+l=[1,2,3,4,5]
+def add (sum,x):
+    return sum+x
+res=reduce(add,l)
+res=reduce(add,l,0)
 print(res)
