1f96ecfa5649fe0682a8c17691660b6a9d4c33ea
Satyanam1 <satya949496@gmail.com>
2026-03-16 21:38:38 +0530

    Added __init__() method

--- oops.py
+++ oops.py
@@ -89,3 +89,29 @@ p3=Myclass()
 print(p1.x)
 print(p2.x)
 print(p3.x)
+
+# __init__() method
+# The __init__() method is used to assign values to object properties, or to perform operations that are necessary when the object is being created.
+
+""" class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+
+print(p1.name)
+print(p1.age) """
+
+""" class Person:
+  def __init__(self, name, age=18):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil")
+p2 = Person("Tobias", 25)
+
+print(p1.name, p1.age)
+print(p2.name, p2.age) """
+
+
