d19cd483c563d8c6666f0c4d672cd04283c73c36
Satyanam1 <satya949496@gmail.com>
2026-03-19 16:03:33 +0530

    add new properties

--- oops.py
+++ oops.py
@@ -183,5 +183,19 @@ p1 = Person("Linus", 30)
 del p1.age
 
 print(p1.name)
-print(p1.age) # This will throw an error
+# print(p1.age) # This will throw an error
+
+# Add new properties
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+p1 = Person("Tobias")
+
+p1.age = 25
+p1.city = "Oslo"
+
+print(p1.name)
+print(p1.age)
+print(p1.city)
 
