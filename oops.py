464e8831c3e99e870559a0f183391e796183f9e6
Satyanam1 <satya949496@gmail.com>
2026-03-22 23:35:42 +0530

    Added Inheritance

--- oops.py
+++ oops.py
@@ -269,3 +269,25 @@ class Person:
 p1 = Person("Tobias", 36)
 print(p1)
 
+# Inheritence 
+
+# Inheritance allows us to define a class that inherits all the methods and properties from another class.
+# Parent class is the class being inherited from, also called base class.
+# Child class is the class that inherits from another class, also called derived class.
+
+# Create a parent class
+# Any class can be a parent class, so the syntax is same as creting any other class.
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("Satyanam", "Ray")
+x.printname()
+
